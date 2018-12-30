c065b5d58c6ec71ad619b2216e02d4870f1ca2be
Rubens <rubens@agenciarosh.com.br>
2018-12-30 15:04:43 -0300

    Sticky header completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,13 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 import $ from 'jquery';
 
 // objects
 var mobileMenu = new MobileMenu();
+var stickyheader = new StickyHeader();
 
+// 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
